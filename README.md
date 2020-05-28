dc111278a338f993c3ad69f6df78b6c4976edf7b
Noah Silvera <noah.a.silvera@gmail.com>
2020-05-28 14:16:53 -0700

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# configure-button-click-3
-Created with CodeSandbox
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { LibraryRegistry } from "@geocortex/web/config";
+import { ComponentType } from "react";
+
+const LAYOUT_NAMESPACE = "custom.a25d74d8";
+
+export default function(registry: LibraryRegistry) {}
